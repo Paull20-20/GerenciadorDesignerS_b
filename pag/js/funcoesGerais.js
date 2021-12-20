34cc13331dd490cfbeff40940a80e8949aafcf2f
Paull20-20 <paulloholiveira77@gmail.com>
2021-12-20 14:14:25 -0300

    first commit

---
+++ pag/js/funcoesGerais.js
@@ -0,0 +1,11 @@
+//funções de testes
+
+//função ao clicar em sair
+function deslogar(){
+    alert("Saindo da sessão....");
+}
+
+//funções para acesso restrito
+function avisoLogar(){
+    alert("Logando no acesso privado, por favor clique em ok!");
+}
